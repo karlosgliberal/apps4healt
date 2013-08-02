56a88e3ff7695c17cb378d7414b6fce94735ffd9
karlos g liberal <karlosgliberal@gmail.com>
2013-08-02 16:41:38 +0200

    Añadir app libre

---
+++ public/scripts/controllers/applibre.js
@@ -0,0 +1,26 @@
+'use strict';
+
+angular.module('apps4healtApp')
+  .controller('AppLibeCtrl', function ($scope, ServicioInscripcion) {
+  	$scope.proyecto = 'App Libre';
+ 		$scope.casa = 'no'; 
+ 		$scope.libre = true;
+		$scope.addMessage = function() {
+			var usuario = {
+      	nombreApellidos: $scope.nombre, 
+      	email: $scope.email,
+      	telefono: $scope.telefono,
+      	alojamiento: $scope.casa,
+      	procedencia: $scope.lugar,
+      	bio: $scope.bio,
+      	proyecto: 'App Libre',
+      	appLibre: ($scope.libre) ? $scope.appLibre : '' 
+    	};
+	    ServicioInscripcion.alta(usuario);
+    	$scope.nombre = "";
+    	$scope.email = "";
+    	$scope.telefono = "";
+    	$scope.bio  = "";   
+  }
+});
+
