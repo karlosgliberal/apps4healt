ac7e7314832ef8268b91c336ca0fb36f4fc8a39d
karlos g liberal <karlosgliberal@gmail.com>
2013-08-05 09:48:17 +0200

    Pruebas correo

--- public/scripts/services/correo.js
+++ public/scripts/services/correo.js
@@ -1,11 +1,12 @@
 'use strict';
 angular.module('EnvioCorreoService',['ngResource']).
 	factory('Correo', function($resource, $location){
-	var Correo = $resource('http://localhost\\:4000/correo',{key:'hola'}, { 
+	var Correo = $resource('http://localhost\\:4000/correo/:id',{}, { 
       	enviar: {
           method: 'POST',
           isArray: true,
-          params: {id:'jjj', hhh:'hggg'}
+          params: {id:'@id'},
+          data:{hola:'hola', adiis:'adios'}
       	}
     	}); 
     	return Correo;
