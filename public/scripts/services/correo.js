285f818fd2ed5961172cee110f7c5995e1b132a8
karlos g liberal <karlosgliberal@gmail.com>
2013-08-05 13:54:51 +0200

    Servicios url

--- public/scripts/services/correo.js
+++ public/scripts/services/correo.js
@@ -4,7 +4,7 @@ angular.module('EnvioCorreoService',[]).
     return {
       enviar: function (usuario){
         $http({
-            url: 'http://46.105.116.39:4000/correo',
+            url: 'http://apps4health.es/correo',
             method: "POST",
             data: usuario,
             //headers: {'Content-Type': 'application/x-www-form-urlencoded'}
